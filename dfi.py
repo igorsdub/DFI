b78b66b42e294b0ab064478ba140ce604ad4eb12
Avishek Kumar <avishek.kumar@asu.edu>
2014-10-23 14:27:59 -0700

    Eliminated old deadcode for the hessian calculation

--- dfi.py
+++ dfi.py
@@ -137,51 +137,7 @@ if __name__ == "__main__":
     print hess 
     exit()
 
-    #compute the Hessian 
-    #compute the Hii terms 
-    for i in len(numres):
-        for j in len(numres):
-            if i==j:
-                continue
-            x_i = x[i]
-            y_i = y[i]
-            z_i = z[i]
-            x_j = x[j]
-            y_j = y[j]
-            z_j = z[j]
-            x_ij = x_i - x_j 
-            y_ij = y_i - y_j 
-            z_ij = z_i - z_j 
-            r = x_ij*x_ij + y_ij*y_ij + z_ij*z_ij 
-            sprngcnst = (gamma*gamma*gamm)/(r*r*r)
-                       
-            #creation of Hii 
-            hess[3*i-2,3*i-2] += sprngcnst*(x_ij*x_ij/r)
-            hess[3*i-1,3*i-1] += sprngcnst*(y_ij*y_ij/r)
-            hess[3*i,3*i] += sprngcnst*(z_ij*z_ij/r)
-
-            hess[3*i-2,3*i-1] += sprngcnst*(x_ij*y_ij/r)
-            hess[3*i-2,3*i] += sprngcnst*(x_ij*z_ij/r)
-            hess[3*i-1,3*i-2] += sprngcnst*(y_ij*x_ij/r)
-            
-            hess[3*i-1,3*i] += sprngcnst*(y_ij*z_ij/r)
-            hess[3*i,3*i-2] += sprngcnst*(x_ij*z_ij/r)
-            hess[3*i,3*i-1] += sprngcnst*(y_ij*z_ij/r)
-            
-            #creation of Hij 
-            hess[3*i-2,3*j-2] -= sprngcnst*(x_ij*x_ij/r)
-            hess[3*i-1,3*j-1] -= sprngcnst*(y_ij*y_ij/r)
-            hess[3*i,3*j] -= sprngcnst*(z_ij*z_ij/r)
-            
-            hess[3*i-2,3*j-1] -= sprngcnst*(x_ij*y_ij/r)
-            hess[3*i-2,3*j] -= sprngcnst*(x_ij*z_ij/r)
-            hess[3*i-1,3*j-2] -= sprngcnst*(y_ij*x_ij/r)
-            
-            hess[3*i-1,3*j] -= sprngcnst*(y_ij*z_ij/r)
-            hess[3*i,3*j-2] -= sprngcnst*(x_ij*z_ij/r)
-            hess[3*i,3*j-1] -= sprngcnst*(y_ij*z_ij/r)
-
-            #need to check is the Hessian is going to work 
+    
     
             
 
