dc15a5e4cf7a2fa146822661bd49abb02142df60
Avishek Kumar <avishek.kumar@asu.edu>
2015-01-12 13:53:17 -0700

    Updated fetchligansight.sh with formatting

--- fetchligandsight.sh
+++ fetchligandsight.sh
@@ -7,6 +7,7 @@
 #input check for file
 
 PROG=$(basename $0)
+LIGFIL="ligands.txt"
 
 usage="
 ==========================
@@ -19,7 +20,8 @@ ${PROG} pdblist.txt
 
 Description 
 -----------
-Fetches ligand binding sights and dumps the output into a text file. 
+Fetches ligand binding sights and dumps the output into a text file. The
+sites are then formatted to be read into dfi in the file $LIGFIL
 
 Example 
 -------
@@ -30,7 +32,11 @@ pdbid.txt:
 
 "
 
-
+if [ -f $LIGFIL ]
+then
+    echo "Deleting the $LIGFIL to start anew."
+    rm -v $LIGFIL
+fi
 
 if [ ! -f "$1" ]
 then
@@ -44,9 +50,7 @@ cat $pdbids | while read f;
 do
     echo "PDBID: $f"
     URL="http://www.ebi.ac.uk/thornton-srv/databases/pdbsum/${f}/grow.out"
-    curl ${URL} > ${f}.out 
+    curl ${URL} > ${f}.out
+    a=$(awk '!/#/ {print $5 " " $14}' ${f}.out | awk '{print $2$1}')
+    echo ${f}:${a} >> ${LIGFIL}
 done
-
-
-
-
