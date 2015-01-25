4af076ccdf367765fbe3981f9b5be67ddc9e2496
Avishek Kumar <avishek.kumar@asu.edu>
2015-01-24 19:35:40 -0700

    Added the fafsaseq.py to align with the fafsa sequence, needs more testing to confirm

---
+++ fafsaseq.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python 
+"""
+=========
+FAFSA SEQ
+=========
+
+Description
+------------
+Downloads the fafsa sequence from the pdb and sees where the sequence begins in
+the fafsa sequence and adds it to the csv file. 
+
+Usage
+-----
+fafsaseq.py DFICSVFILE
+"""
+
+import pandas as pd
+import numpy as np 
+import sys 
+
+mapres={}
+with open('RESdict.txt','r') as infile:
+    for line in infile:
+        ls_line=line.strip('\n').split(':')
+        mapres[ls_line[0]] = ls_line[1]
+
+#fname='1iau-dfianalysis.csv'
+fname=sys.argv[1]
+data = pd.read_csv(fname)
+
+bigseq=np.array(data['Res'].values,dtype=str)
+smallseq=''
+for threeseq in bigseq:
+    smallseq = smallseq + str(mapres[threeseq])
+
+import urllib2
+URL="http://www.pdb.org/pdb/files/fasta.txt?structureIdList="
+#pdbname="1IAU"
+pdbname=fname.split('-')[0]
+response = urllib2.urlopen(URL+pdbname)
+html = response.read()    
+html = html.split('\n')
+fseq=''
+for line in html:
+    print line
+    if line.startswith('>'):
+        continue
+    fseq = fseq + line
+
+for i in range(len(fseq)):
+    print fseq[i:i+10], smallseq[:10]
+    if smallseq[:10] == fseq[i:i+10]:
+        match = i+1
+        print "Found a match at %d"%(i+1)
+        break
+
+data['fafsa']=pd.Series( range(match,len(smallseq)+match), index=data.index)
+
+data.to_csv(fname)
