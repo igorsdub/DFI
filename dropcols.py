bb56c1e98b2341425256ebcfaeb83d32fff243ef
Avishek Kumar <avishek.kumar@asu.edu>
2015-01-30 12:19:09 -0700

    Updated dropcols to look for columns name first

--- dropcols.py
+++ dropcols.py
@@ -16,13 +16,14 @@ if len(sys.argv) < 2:
 
 def drop_cols():
     import pandas as pd
+    ls_dropcols = ['Unnamed: 0', 'Unnamed: 0.1', 'Unnamed: 0.1']
 
     fname=sys.argv[1]
     pd.read_csv(fname,index_col='ResI')
     data=pd.read_csv(fname,index_col='ResI')
-    data.drop('Unnamed: 0', axis=1, inplace=True)
-    drop.drop('Unnamed: 0.1', axis=1, inplace=True)
-    data.drop('Unnamed: 0.1', axis=1, inplace=True)
+    for dc in ls_dropcols:
+        if dc in data.columns:
+            data.drop(dc, axis=1, inplace=True)
     data.to_csv(fname)
 
 if __name__ == "__main__":
