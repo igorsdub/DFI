817c2110922bbe7270c4f6a1172dca2fb4ecf7f0
Avishek Kumar <avishek.kumar@asu.edu>
2016-03-08 13:32:15 -0700

    Renamed fafsaconvert to fasta_convert.py and added docstrings

--- fasta_convert.py
+++ fasta_convert.py
@@ -1,8 +1,13 @@
 # IPython log file
+"""
+Fasta Convert 
+-------------
 
+Suite of Tools for converting to and from the 
+fasta sequence. 
+"""
 import pdbio
 import glob 
-fname = glob.glob('*.pdb')
 
         
 mapres={'ALA':'A',
@@ -27,23 +32,61 @@ mapres={'ALA':'A',
 'VAL':'V'}
     
 def get_seq(fname):
+    """
+    Get the structural sequence from a PDB file. 
+    
+    Input
+    -----
+    fname: file
+       name of pdb file 
+    """
     ATOMS=[]
     pdbio.pdb_reader(fname,ATOMS,CAonly=True,noalc=True,Verbose=False)
     for a in ATOMS:
         yield mapres[a.res_name]
         
 def format_seq(seq):
+    """
+    Format sequence so it is every 100 lines
+    
+    Input
+    -----
+    seq: str 
+       String of the protein's sequence. 
+
+    Output
+    ------
+    seq_str: str 
+       Formatted Sequence 
+    """
     i,seq_str = 0,''
     for s in seq:
         seq_str = seq_str + s
-        i+=1
+        i+=1 #TODO use textwrap library instead 
         if(i == 100):
             seq_str = seq_str+'\n'
             i = 0 
     return seq_str
 
 def fafsa_format(fname,outfileobj=None):
-    title = fname.split('_')[0]
+    """
+    Converts a sequence in pdb format into 
+    fafsa format. 
+
+    Input
+    -----
+    fname: file
+       Name of pdb file 
+    outfileobj: file object 
+       file object to write out to can be a filename
+       or just stdout
+
+    Output
+    ------
+    fafsafmt: str
+       fafsa format of pdb file. 
+    """
+    title = fname.split('_')[0] #could lead to a bug 
     seq_str = format_seq( [x for x in get_seq(fname) ] )
     fafsafmt="""
 >{}| | |len={}
@@ -54,6 +97,17 @@ def fafsa_format(fname,outfileobj=None):
         print fafsafmt 
 
 def separate_fasta(fname):
+    """
+    Given a filename with multiple fasta sequences 
+    this function will separate them and write them 
+    to individual files labeled the title of the fasta
+    sequence.fasta
+
+    Input 
+    -----
+    fname: file
+       File containing multiple fasta sequences 
+    """
     outfile = None 
     with open(fname,'r') as infile:
         for line in infile:
