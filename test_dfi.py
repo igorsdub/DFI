f8ac7a4df9ae06f2a8cf879cc134b4ba9569c84a
Avishek Kumar <avishek.kumar@asu.edu>
2016-03-14 23:37:01 -0700

    Fixed pdbreader problem and moved 1l2y.pdb to the data folder

--- test_dfi.py
+++ test_dfi.py
@@ -11,8 +11,8 @@ def test_pctrank_descending():
     assert np.all( dfi_calc.pctrank(a,inverse=True) == np.array([1.,0.8,0.6,0.4,0.2],dtype=float) )
 
 def test_comlineargs():
-    comline='./dfi.py --fdfi A19 A10 --hess mwcovar.dat --pdb 1l2y.pdb'
+    comline='./dfi.py --fdfi A19 A10 --hess mwcovar.dat --pdb ./data/1l2y.pdb'
     dict_parms = dfi_calc.CLdict( comline.split() )
-    assert dict_parms['--pdb'] == '1l2y.pdb'
+    assert dict_parms['--pdb'] == './data/1l2y.pdb'
     assert dict_parms['--hess'] == 'mwcovar.dat'
     assert np.all( dict_parms['--fdfi'] == np.array(['A19', 'A10']) )
