66fad1fa69051c4b5b3d2fcbd6a4ba84c17305fb
Avishek Kumar <avishek.kumar@asu.edu>
2016-03-23 14:54:07 -0700

    Added setup tools

---
+++ setup.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python
+"""Setuptools-based setup script for DFI.
+For a basic installation just type the command::
+  python setup.py install
+"""
+
+from setuptools import setup
+
+setup(name='dfi',
+      version='0.1.0',
+      description='Dynamic Flexibility Index',
+      author='Avishek Kumar',
+      author_email='avishek.kumar@asu.edu',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+      packages=['dfi'],
+      scripts=[],
+      license='BSD',
+      long_description=open('README.md').read(),
+      install_requires=['numpy', 'pandas', 'pytest', 'matplotlib', 'biopython',
+                        'scipy','seaborn']
+      )
