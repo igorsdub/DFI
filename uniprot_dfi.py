240fc09ddff4d2c7dc3f6fc3bd9d442b7a52f54f
Avishek Kumar <avishek.kumar@asu.edu>
2016-03-23 01:15:34 -0700

    Checking in __init__.py

--- uniprot_dfi.py
+++ uniprot_dfi.py
@@ -17,26 +17,10 @@ Example
 ./dfi.py P42771
 
 """
-
-import UniproBlastToPdb as uni
 import sys 
+import UniproBlastToPdb as uni
+from download_pdb import fetch_pdb 
 import dfi_calc 
-
-def fetch_pdb(id,Verbose=False):
-    """
-    Download pdb file and write out to file id.pdb  
-    
-    Input
-    ------
-    id: str
-       4 letter pdb code 
-    """
-    import urllib 
-    url = 'http://www.rcsb.org/pdb/files/%s.pdb' % id
-    with open(id+'.pdb','w') as outfile: 
-        outfile.write(urllib.urlopen(url).read())
-    if(Verbose):
-        print("Wrote out %s.pdb"%(id))
 
 def uniproDFI(uniprotcodes):
     """
