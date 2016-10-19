a50445b6a6a178516618f9ae484718386343e467
Avishek Kumar <avishek.kumar@outlook.com>
2016-10-18 22:55:35 -0500

    added requirements.txt to setup.py closes #2

--- setup.py
+++ setup.py
@@ -6,11 +6,36 @@ For a basic installation just type the command::
 
 from setuptools import setup
 
+
+def parse_requirements(fname):
+    """
+    Parses requirments.txt file into a list
+    
+    Parameters
+    ----------
+    fname: str
+        name of the file with dependencies
+        (e.g., requirements.txt)
+
+    Returns
+    -------
+    dependencies: ls[str]
+       ls of dependencies
+    """
+    with open(fname, 'r') as infile:
+        dependencies = (
+        infile.read().splitlines()
+        )
+
+    return dependencies
+
+
+
 setup(name='dfi',
       version='0.1.0',
       description='Dynamic Flexibility Index',
       author='Avishek Kumar',
-      author_email='avishek.kumar@asu.edu',
+      author_email='avishek@asu.edu',
       classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -22,9 +47,13 @@ setup(name='dfi',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
       packages=['dfi'],
-      scripts=['./dfi/dfi_calc.py','./dfi/uniprot_dfi.py','./dfi/fastaseq.py'],
+      scripts=['./dfi/dfi_calc.py',
+               './dfi/uniprot_dfi.py',
+               './dfi/fastaseq.py'],
       license='BSD',
       long_description=open('README.md').read(),
-      install_requires=['numpy', 'pandas', 'pytest', 'matplotlib', 'biopython',
-                        'scipy','seaborn']
+      install_requires=parse_requirements('requirements.txt')
       )
+
+
+
